9b811abca4b51cefd732224febea4582037210bd
Chintamani Raju Virulkar <chintamani.virulkar@gmail.com>
2021-07-30 19:07:13 +0530

    scrapped economical news from CNN RSS fedd

--- CNN RSS feed Scrapping/Economical/main.py
+++ CNN RSS feed Scrapping/Economical/main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = 'http://rss.cnn.com/rss/edition.rss'
+url = 'http://rss.cnn.com/rss/money_news_international.rss'
 res = requests.get(url)
 soup = BeautifulSoup(res.content, features='html.parser')
 print(soup.prettify())
@@ -11,12 +11,9 @@ headline = []
 pub_date = []
 des = []
 for item in items:
-    if item.title in item:
-        headline.append(item.title.text)
-    if item.pubdate in item:
-        pub_date.append(item.pubdate.text)
-    if item.description in item:
-        des.append(item.description.text)
+    headline.append(item.title.text)
+    pub_date.append(item.pubdate.text)
+    des.append(item.description.text)
 
 df = pd.DataFrame(list(zip(headline, pub_date, des)), columns=['Headline','Publish Date','Description'])
-df.to_csv('D:/Epicenter-News-Research/CNN RSS feed Scrapping/Top Stories/top_stories.csv')
+df.to_csv('D:/Epicenter-News-Research/CNN RSS feed Scrapping/Economical/economical.csv')
